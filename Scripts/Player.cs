3b21e7845aadab984f2fa4a207f45300de9e0fab
PSokol <williamsokol0@gmail.com>
2023-01-15 02:05:30 -0800

    added stuff

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,34 +3,48 @@ using System;
 
 public class Player : Godot.KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+
+    public Sprite sprite;
+    [Export]
+    Texture open ;//= (Texture)GD.Load("res://image.png"); 
+    [Export]
+    Texture close ;
     [Export]
     public float speed = 1;
+    [Export]
+    public float flapDelay = .5f;
 
     // Called when the node enters the scene tree for the first time.
    
     public override void _Ready()
     {
-        
+        sprite = (Sprite)this.GetNode("Sprite");
         Position = GetViewportRect().Size/2;
         //this.Position = new Vector2(100,100);
+        FlapAnim();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
  public override void _Process(float delta)
  {
     float dir = Input.GetAxis("Left","Right");
-    GD.Print(dir);
+    if(dir != 0){
+        flapDelay = .2f;
+    }else{
+        flapDelay = .6f;
+    }
     this.Position = new Vector2(this.Position.x+(dir*speed),this.Position.y);   
  }
-//  public async void FlapAnim()
-//  {
-//     var messageTimer = GetNode<Timer>("MessageTimer");
-//     // while(true)
-//     // {
-//         await ToSignal(messageTimer, "timeout");
-//     // }
-//  }
+ public async void FlapAnim()
+ {
+    //var messageTimer = GetNode<Timer>("MessageTimer");
+    GD.Print("test1");
+    while(true)
+    {
+        sprite.Texture = open;
+        await ToSignal(GetTree().CreateTimer(flapDelay), "timeout");
+        sprite.Texture = close;
+        await ToSignal(GetTree().CreateTimer(flapDelay), "timeout");
+    }
+ }
 }
